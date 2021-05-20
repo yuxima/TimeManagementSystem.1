9284ae330ff3d154445e0663e555a89c3fdfca65
Nastia Yukhymchuk <yuximchuk2111@gmail.com>
2021-05-21 00:05:39 +0300

    Added neccessary projects and folders

---
+++ TimeManagementSystem.Data/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeManagementSystem.Data.Entities
+{
+    class BaseEntity
+    {
+    }
+}
---
+++ TimeManagementSystem.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TimeManagementSystem.Service
+{
+    public class Class1
+    {
+    }
+}
